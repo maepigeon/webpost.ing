84cd145402ddafc3e955cf78338a37a268ced897
Mae Pigeon <maepigeon1217@gmail.com>
2025-03-16 00:55:40 -0400

    Improved ContentEditable usage and increased post body maximum length.

--- config/database.sql
+++ config/database.sql
@@ -1,6 +1,9 @@
 CREATE TABLE posts (
     "id" SERIAL PRIMARY KEY,
     "title" VARCHAR(255),
-    "description" VARCHAR(255),
-    "published" BOOL NOT NULL
+    "description" VARCHAR(16383),
+    "published" BOOL NOT NULL,
+    "date" TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+DROP TABLE posts;
